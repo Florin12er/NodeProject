112cec95df5ca43f56d3e1444dc219dac37d3a19
florin <apetreiflorin236@gmail.com>
2024-06-08 19:01:35 +0200

    connected to the database and made the initial setup

---
+++ server.cjs
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const Routes = require("./routes/index.cjs");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use("/", Routes);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running...");
+});
+
