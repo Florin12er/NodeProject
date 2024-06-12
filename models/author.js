dc2630ecd82c49761c2b5eea8f06ae54c5839dcb
florin <apetreiflorin236@gmail.com>
2024-06-12 10:16:31 +0200

    added the ability to remove authors and added a view page

---
+++ models/author.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Book = require("./book");
+
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+authorSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next, doc) {
+    try {
+      const books = await Book.find({ author: doc._id });
+      if (books.length > 0) {
+        next(new Error("This author has books still"));
+      } else {
+        next();
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+module.exports = mongoose.model("Author", authorSchema);
--- server.js
+++ server.js
@@ -3,40 +3,33 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
-const Routes = require("./routes/index.cjs");
-const Authors = require("./routes/authors.cjs");
-const Error = require("./routes/404.cjs");
-const Books = require("./routes/books.cjs")
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
-
-// Set up view engine and static files
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({limit: "10mb", extended: false}))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("MongoDB Connected"));
+db.once("open", () => console.log("Connected to Mongoose"));
 
-
-// Route setup
-app.use("/", Routes);
-app.use("/authors", Authors);
-app.use("/books", Books)
-
-// 404 Error handler as middleware
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 app.use((req, res, next) => {
     res.status(404).render("404", { layout: false });
 });
-
-
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running...");
 });
